4b7bef87482683abc0af7fe196254a97bf9c3c94
Dennis Schmidt <dmrschmidt@gmail.com>
2018-04-13 15:36:01 -0500

    semi constrained movements

--- src/dropper.js
+++ src/dropper.js
@@ -4,8 +4,30 @@
     var dropZone = document.getElementById('drop-zone');
     var uploadForm = document.getElementById('js-upload-form');
 
-    var startUpload = function(files) {
+    var constrainMovement = function(e) {
+      var target = $(e.target)
+      if (target.position().left < 0) {
+        target.css({left: 0})
+        return false
+      }
+      if (target.position().top < 0) {
+        target.css({top: 0})
+        return false
+      }
+      // console.log(target.position().left)
+      // console.log(target.width())
+      // console.log($('.dragger').position().left)
+      // console.log($('.dragger').width())
+      // console.log("end")
+      console.log($('.dragger').position().left + target.width())
+      if ($('.dragger').position().left + $('.dragger').width() > $('#creator').position().left + $('#creator').width()) {
+        target.css({left: 0})
+        return false
+      }
+      return true
+    }
 
+    var startUpload = function(files) {
         // Loop through the FileList and render image files as thumbnails.
         for (var i = 0, f; f = files[i]; i++) {
 
@@ -24,13 +46,13 @@
               start: function() {
                 $('.dragging-preview').addClass('dragging')
               },
-              drag: function() {
-                // return false here to prevent further movement
+              drag: function(e) {
+                return constrainMovement(e)
               },
               stop: function() {
                 $('.dragging-preview').removeClass('dragging')
-              }/*, containment: "#drop-target", scroll: false */});
-              $('.drag-container').addClass('dragging')
+              }});
+              $('.drag-container').addClass('draggable')
             };
           })(f);
 
