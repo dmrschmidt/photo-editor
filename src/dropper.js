ae69355a4e7a7f848f6c6baec6fd157e77b65be1
Dennis Schmidt <dmrschmidt@gmail.com>
2018-04-13 14:45:36 -0500

    proper border around image

--- src/dropper.js
+++ src/dropper.js
@@ -1,9 +1,6 @@
 + function($) {
     'use strict';
 
-    // UPLOAD CLASS DEFINITION
-    // ======================
-
     var dropZone = document.getElementById('drop-zone');
     var uploadForm = document.getElementById('js-upload-form');
 
@@ -19,19 +16,17 @@
 
           var reader = new FileReader();
 
-          // Closure to capture the file information.
           reader.onload = (function(theFile) {
             return function(e) {
-              // Render thumbnail.
-              // var span = document.createElement('span');
               document.getElementById('drop-content').innerHTML = ['<img class="" src="', e.target.result,
                                 '" title="', escape(theFile.name), '" id="dropped"/>'].join('');
-              $('.drag-container').draggable({handle: '.dragger'});
-              // document.getElementById('drop-target').insertBefore(span, null);
+              $('.drag-container').draggable({handle: '.dragger', drag: function() {
+                // return false here to prevent further movement
+              }/*, containment: "#drop-target", scroll: false */});
+              $('.drag-container').addClass('dragging')
             };
           })(f);
 
-          // Read in the image file as a data URL.
           reader.readAsDataURL(f);
         }
       }
