cc68c6acf96a4de1cfab93ecc3ebbf408e1e06fa
Dennis Schmidt <dmrschmidt@gmail.com>
2018-04-14 10:43:47 -0500

    all direction resizing [#156777352]

--- src/resizer.js
+++ src/resizer.js
@@ -44,38 +44,28 @@
     var handleMouseMove = function(e) {
         if (!document.isResizing) { return }
 
-        var direction = dragTarget.attr('class').split(" ").filter(function(c) { return c.startsWith("ord-") })[0]
-        switch (direction) {
-            case "ord-w": {
-                console.log("direction west")
-                var movement = e.pageX - initialOffset.left
-                dragger.css({'left': initialPosition.left + movement})
-                dragger.css({'width': initialSize.width - movement})
-                break
-            }
-            case "ord-e": {
-                console.log("direction east")
-                var movement = e.pageX - initialOffset.left
-                dragger.css({'width': initialSize.width + movement})
-                break
-            }
-            case "ord-n": {
-                console.log("direction north")
-                var movement = e.pageY - initialOffset.top
-                dragger.css({'top': initialPosition.top + movement})
-                dragger.css({'height': initialSize.height - movement})
-                break
-            }
-            case "ord-s": {
-                console.log("direction south")
-                var movement = e.pageY - initialOffset.top
-                dragger.css({'height': initialSize.height + movement})
-                break
-            }
-            default:{
-                console.log("other direction:" + direction + ":")
-                break
-            }
+        var movementX = e.pageX - initialOffset.left
+        var movementY = e.pageY - initialOffset.top
+
+        var ord = dragTarget.attr('class').split(" ").filter(function(c) { return c.startsWith("ord-") })[0]
+        var direction = ord.split("-")[1]
+        if (/w/.test(direction)) {
+            console.log("direction west")
+            dragger.css({'left': initialPosition.left + movementX})
+            dragger.css({'width': initialSize.width - movementX})
+        }
+        if (/e/.test(direction)) {
+            console.log("direction east")
+            dragger.css({'width': initialSize.width + movementX})
+        }
+        if (/n/.test(direction)) {
+            console.log("direction north")
+            dragger.css({'top': initialPosition.top + movementY})
+            dragger.css({'height': initialSize.height - movementY})
+        }
+        if (/s/.test(direction)) {
+            console.log("direction south " + direction + ", " + /s/.test(direction))
+            dragger.css({'height': initialSize.height + movementY})
         }
     }
 
